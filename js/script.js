bc15f3ba91f111a27f1cdc3f845568472ea17946
Youri Maratkanov <Maratkanov.IUS@nsd.ru>
2018-11-16 11:27:54 +0300

    еще правки

--- js/script.js
+++ js/script.js
@@ -3,11 +3,19 @@ $(document).ready(function(){
 	$input_block = $('.input-block');
 	console.log('Родительский блок: ' + $parent_block.width() + ' пикселей');
 
-	if($parent_block >= 1600){
+	if($parent_block.width() >= 1600){
 		$input_block.css({'width':'480px', 'margin-left':'32px'});
 	}
 
-	if($parent_block >= 1024 && $parent_block <= 1599){
+	if($parent_block.width() >= 1024 && $parent_block.width() <= 1599){
 		$input_block.css({'width':'30%', 'margin-left':'2%'});
+	}
+
+	if($parent_block.width() >= 640 && $parent_block.width() <= 1023){
+		$input_block.css({'width':'44%', 'margin-left':'4%'});
+	}
+
+	if($parent_block.width() <=639){
+		$input_block.css({'width':'80%', 'margin-left':'10%'});
 	}
 });
